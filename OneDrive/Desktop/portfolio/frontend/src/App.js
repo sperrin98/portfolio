90b1cce34ba6baba065cb63ad2f73747d246a34d
sperrin98 <stanperrin98@gmail.com>
2024-08-01 12:35:32 +0100

    basic overview

---
+++ OneDrive/Desktop/portfolio/frontend/src/App.js
@@ -0,0 +1,26 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <section className="section" id="overview">
+        <h1>Overview</h1>
+      </section>
+      <section className="section" id="about-me">
+        <h1>About me</h1>
+      </section>
+      <section className="section" id="work-projects">
+        <h1>Work / Projects</h1>
+      </section>
+      <section className="section" id="skills">
+        <h1>Skills</h1>
+      </section>
+      <section className="section" id="contact-me">
+        <h1>Contact me</h1>
+      </section>
+    </div>
+  );
+}
+
+export default App;
