3a1f5d68010d1a2b01ebbbb8b0d937c2bdf4c50b
sperrin98 <stanperrin98@gmail.com>
2024-08-01 16:13:40 +0100

    background

--- OneDrive/Desktop/portfolio/frontend/src/App.js
+++ OneDrive/Desktop/portfolio/frontend/src/App.js
@@ -1,11 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import overviewImage from './assets/overview-bg.png'; // Adjust path based on where you place the image
 
 function App() {
   return (
     <div className="App">
-      <section className="section" id="overview">
-        <h1>Overview</h1>
+      <section
+        className="section"
+        id="overview"
+        style={{
+          backgroundImage: `url(${overviewImage})`, // Use the correct property and path
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <div className="overview-container">
+          <h3 className="intro">Hi, my name is...</h3>
+          <h2 className="my-name">Stanley Perrin</h2>
+          <h1 className="job-title">I am a web developer</h1>
+        </div>
       </section>
       <section className="section" id="about-me">
         <h1>About me</h1>
